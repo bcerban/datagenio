df7640743e252167579cb41802506d96a7ccbce0
Bettina Cerban <bettinacerban@gmail.com>
2019-01-03 13:20:36 -0300

    update state definition

--- crawler/src/main/java/com/datagenio/crawler/api/State.java
+++ crawler/src/main/java/com/datagenio/crawler/api/State.java
@@ -2,13 +2,16 @@ package com.datagenio.crawler.api;
 
 import org.jsoup.nodes.Document;
 
+import java.net.URI;
 import java.util.Collection;
 
 public interface State {
 
+    URI getUri();
     Document getDocument();
     Collection<Eventable> getEventables();
 
+    void setUri(URI uri);
     void setDocument(Document document);
     void setEventables(Collection<Eventable> eventables);
 
--- crawler/src/main/java/com/datagenio/crawler/model/StateImpl.java
+++ crawler/src/main/java/com/datagenio/crawler/model/StateImpl.java
@@ -5,30 +5,47 @@ import com.datagenio.crawler.api.EventableExtractor;
 import com.datagenio.crawler.api.State;
 import org.jsoup.nodes.Document;
 
+import java.net.URI;
 import java.util.Collection;
 
 public class StateImpl implements State {
 
     private Collection<Eventable> eventables;
     private Document document;
+    private URI uri;
 
-    public StateImpl(Document view, EventableExtractor extractor) {
+    public StateImpl(URI uri, Document view, EventableExtractor extractor) {
+        this.uri = uri;
         this.document = view;
         this.eventables = extractor.extract(this, this.document);
     }
 
+    @Override
     public Collection<Eventable> getEventables() {
         return eventables;
     }
 
+    @Override
     public void setEventables(Collection<Eventable> eventables) {
         this.eventables = eventables;
     }
 
+    @Override
+    public URI getUri() {
+        return this.uri;
+    }
+
+    @Override
+    public void setUri(URI uri) {
+        this.uri = uri;
+    }
+
+    @Override
     public Document getDocument() {
         return document;
     }
 
+    @Override
     public void setDocument(Document document) {
         this.document = document;
     }
