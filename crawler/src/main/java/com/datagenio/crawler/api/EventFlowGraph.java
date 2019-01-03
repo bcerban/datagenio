08f6c94f3ed44c9ab8977a368b1992b04f067b89
Bettina Cerban <bettinacerban@gmail.com>
2019-01-03 15:25:49 -0300

    add eventflowgraph

---
+++ crawler/src/main/java/com/datagenio/crawler/api/EventFlowGraph.java
@@ -0,0 +1,15 @@
+package com.datagenio.crawler.api;
+
+
+import java.util.Collection;
+
+public interface EventFlowGraph {
+
+    Collection<State> getStates();
+    Collection<Transitionable> getTransitions();
+    Collection<Eventable> getEvents();
+
+    void addState(State state);
+    void addTransition(Transitionable transition);
+    void addEvent(Eventable event);
+}
